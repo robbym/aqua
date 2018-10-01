7d24bfeb8dd131550106651ac192bc1c04a84eb6
Robby Madruga <robbymadruga@gmail.com>
2018-09-30 22:57:03 -0700

    Moved parse example to ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -24,3 +24,27 @@ pub struct RegisterAST {
     pub name: String,
     pub register_fields: Vec<RegisterFieldAST>
 }
+
+
+    // let parser = aqua::RegisterParser::new().parse("
+    // register RCC {
+    //     u8 {
+    //         REG1<1:0>;
+    //         REG2<3:2>;
+    //         REG3<5:4>;
+    //         REG4<7:6>;
+    //     }
+    //     u16 {
+    //         REG1<1:0>;
+    //         REG2<3:2>;
+    //         REG3<5:4>;
+    //         REG4<7:6>;
+    //     }
+    //     u32 {
+    //         REG1<1:0>;
+    //         REG2<3:2>;
+    //         REG3<5:4>;
+    //         REG4<7>;
+    //     }
+    // }").unwrap();
+    // println!("{:#?}", parser);
